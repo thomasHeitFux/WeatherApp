62f7ecc9b2d5bcf0279df08d5baf3f379de2a8e1
Thomas Dominguez <90942448+thomasHeitFux@users.noreply.github.com>
2022-10-02 23:24:07 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home () {
   const apiKey ='4ae2636d8dfbdc3044bede63951a019b'
 
   function onSearch(ciudad) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
